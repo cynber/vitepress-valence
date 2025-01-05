c9e2673039d7c33f5760b204ace9a5fcd518baf6
cynber <26402139+cynber@users.noreply.github.com>
2025-01-05 04:38:20 -0800

    prepare for 0.1.2
    - use default node .gitignore
    - bundle Vue components for import

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        sourcemap: true,
+        lib: {
+            entry: path.resolve(__dirname, 'index.js'),
+            name: 'VitepressComponents',
+            fileName: (format) => `vitepress-components.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['vue', 'vitepress', '@iconify/vue'],
+            output: {
+                globals: {
+                    vue: 'Vue',
+                    vitepress: 'VitePress',
+                    '@iconify/vue': 'IconifyVue',
+                },
+            },
+        },
+    },
+});
