462f62fb68df62998f59374fa3e988433e54ec1f
cynber <26402139+cynber@users.noreply.github.com>
2025-01-26 06:36:12 -0800

    extract styles to main.scss

--- src/components/containers/HorizontalContainer.vue
+++ src/components/containers/HorizontalContainer.vue
@@ -8,16 +8,13 @@
 
 </script>
 
-<style scoped>
+<style lang="scss">
+@use "@/assets/main.scss";
+
 .cards-container {
+  @extend %cards-container;
   display: flex;
   flex-direction: column;
   gap: 2rem;
-  padding: 16px;
-  margin: 16px auto;
-  border-radius: 8px;
-  border: 1px solid var(--vp-c-divider);
-  background-color: var(--vp-c-bg-soft);
-  box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.05);
 }
 </style>
--- src/components/containers/VerticalContainer.vue
+++ src/components/containers/VerticalContainer.vue
@@ -4,20 +4,16 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
 
-<style scoped>
+<style lang="scss">
+@use "@/assets/main.scss";
+
 .cards-container {
+  @extend %cards-container;
   display: flex;
   flex-wrap: wrap;
   gap: 1.5rem;
-  padding: 16px;
-  margin: 16px auto;
   justify-content: center;
-  border-radius: 8px;
-  background-color: var(--vp-c-bg-soft);
-  border: 1px solid var(--vp-c-divider);
-  box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.05);
 }
 
 @media screen and (max-width: 1024px) {
