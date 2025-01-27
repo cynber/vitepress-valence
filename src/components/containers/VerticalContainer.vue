124ca08121f8ad1d85bccdbe2763ad7814b7c455
cynber <26402139+cynber@users.noreply.github.com>
2025-01-27 09:53:12 -0800

    revert buggy CSS

--- src/components/containers/VerticalContainer.vue
+++ src/components/containers/VerticalContainer.vue
@@ -5,17 +5,19 @@
 </template>
 
 
-<style lang="scss">
-@use "@/assets/main.scss";
-
+<style scoped>
 .cards-container {
-  @extend %cards-container;
   display: flex;
   flex-wrap: wrap;
   gap: 1.5rem;
+  padding: 16px;
+  margin: 16px auto;
   justify-content: center;
+  border-radius: 8px;
+  background-color: var(--vp-c-bg-soft);
+  border: 1px solid var(--vp-c-divider);
+  box-shadow: inset 0 1px 4px rgba(0, 0, 0, 0.05);
 }
-
 @media screen and (max-width: 1024px) {
   .cards-wrapper {
     gap: 1rem;
